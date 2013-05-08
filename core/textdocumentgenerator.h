3e2699960e6e82914e78e36ba10b115e5051cb2d
Azat Khuzhin <a3at.mail@gmail.com>
2013-05-08 08:08:30 +0400

    Fix comment for what configName is need in textdocumentgenerator

--- core/textdocumentgenerator.h
+++ core/textdocumentgenerator.h
@@ -147,7 +147,7 @@ class OKULAR_EXPORT TextDocumentGenerator : public Generator, public Okular::Con
         /**
          * Creates a new generator that uses the specified @p converter.
          *
-         * @param configName - see Okular::TextDocumentSettings
+         * @param configName - see Okular::TextDocumentSettingsSkeleton
          *
          * @note the generator will take ownership of the converter, so you
          *       don't have to delete it yourself
